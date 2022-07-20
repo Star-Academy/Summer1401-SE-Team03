f9a5f72ea0f57cf73482595345f264b98e8b07aa
marzieafshar <m.afshar.ce@gmail.com>
2022-07-20 11:10:05 +0430

    finish phase 1

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ public class Main {
     public static void main(String[] args) {
         readStopWords();
         readAllFilesAndSaveWords();
-//        Collections.sort(mapOfWords);
         handleInput();
     }
 
@@ -21,8 +20,8 @@ public class Main {
         int input = sc.nextInt();
         if (input == 1)
             handleNumber1();
-//        else if (input == 2)
-//            handleNumber2();
+        else if (input == 2)
+            handleNumber2();
     }
 
     private static void handleNumber1() {
@@ -32,81 +31,87 @@ public class Main {
         System.out.println(mapOfWords.get(word));
     }
 
-//    private static void handleNumber2() {
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Please enter a series of words:");
-//        String[] words = sc.nextLine().toLowerCase().split(" ");
-//
-//        ArrayList<String> neutrals = new ArrayList<>();
-//        ArrayList<String> positives = new ArrayList<>();
-//        ArrayList<String> negatives = new ArrayList<>();
-//
-//        for (String word : words) { // Dividing words into 3 groups
-//            if (word.startsWith("+"))
-//                positives.add(word.replaceFirst("\\+", ""));
-//            else if (word.startsWith("-"))
-//                negatives.add(word.replaceFirst("-", ""));
-//            else neutrals.add(word);
-//        }
-////        System.out.println("positives are: "+ positives);
-////        System.out.println("negatives are: "+ negatives);
-////        System.out.println("neutrals are: "+ neutrals);
-//
-//        System.out.println(findingTheResult(neutrals, positives, negatives));
-//    }
-//
-//    private static ArrayList<Integer> findingTheResult(ArrayList<String> neutrals, ArrayList<String> positives, ArrayList<String> negatives) {
-//        ArrayList<Integer> result = new ArrayList<>();
-//        for (String neutral : neutrals) {
-//            if (mapOfWords.get(neutral) == null) {
-//                result.clear();
-//                break;
-//            } else {
-//                intersectionOverArrayLists(result, mapOfWords.get(neutral));
-//                if (result.isEmpty())
-//                    break;
-//            }
-//        } // So far, we have found the intersection of neutrals!
-//
-//        if (!result.isEmpty()) {
-//            ArrayList<Integer> unionOfPositives = new ArrayList<>();
-//            ArrayList<Integer> unionOfNegatives = new ArrayList<>();
-//            for (String positive : positives) { // finding the union of positives
-//                if (mapOfWords.get(positive) != null) {
-//                    unionOverArrayLists(unionOfPositives, mapOfWords.get(positive));
-//                }
-//            }
-//            for (String negative : negatives) { // finding the union of negatives
-//                if (mapOfWords.get(negative) != null) {
-//                    unionOverArrayLists(unionOfNegatives, mapOfWords.get(negative));
-//                }
-//            }
-//            intersectionOverArrayLists(result, unionOfPositives);
-//            subtractionFromArrayLists(result, unionOfNegatives);
-//        }
-//        return result;
-//    }
+    private static void handleNumber2() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a series of words:");
+        String[] words = sc.nextLine().toUpperCase().split(" ");
 
-//    private static void intersectionOverArrayLists(ArrayList<Integer> result, ArrayList<Integer> addee) {
-//        for (int fileNumber : result) {
-//            if (!addee.contains(fileNumber))
-//                result.remove(fileNumber);
-//        }
-//    }
-//
-//    private static void unionOverArrayLists(ArrayList<Integer> result, ArrayList<Integer> addee) {
-//        for (int fileNumber : addee) {
-//            if (!result.contains(fileNumber))
-//                result.add(fileNumber);
-//        }
-//    }
+        ArrayList<String> neutrals = new ArrayList<>();
+        ArrayList<String> positives = new ArrayList<>();
+        ArrayList<String> negatives = new ArrayList<>();
 
-//    private static void subtractionFromArrayLists(ArrayList<Integer> result, ArrayList<Integer> removee) {
-//        for (int fileNumber : removee) {
-//            if (result.contains(fileNumber))
-//                result.remove(fileNumber);
-//        }
-//    }
+        for (String word : words) { // Dividing words into 3 groups
+            if (word.startsWith("+"))
+                positives.add(word.replaceFirst("\\+", ""));
+            else if (word.startsWith("-"))
+                negatives.add(word.replaceFirst("-", ""));
+            else neutrals.add(word);
+        }
+        System.out.println(findingTheResult(neutrals, positives, negatives));
+    }
+
+    private static ArrayList<Integer> findingTheResult(ArrayList<String> neutrals, ArrayList<String> positives, ArrayList<String> negatives) {
+        ArrayList<Integer> result = new ArrayList<>();
+        boolean isFirstTime = true;
+        for (String neutral : neutrals) {
+            if (!mapOfWords.containsKey(neutral)) {
+                result.clear();
+                break;
+            } else {
+                if (isFirstTime) {
+                    result = mapOfWords.get(neutral);
+                    isFirstTime = false;
+                } else {
+                    result = getIntersectionOverArrayLists(result, mapOfWords.get(neutral));
+                    if (result.isEmpty())
+                        break;
+                }
+            }
+        } // So far, we have found the intersection of neutrals!
+
+        if (!result.isEmpty()) {
+            ArrayList<Integer> unionOfPositives = new ArrayList<>();
+            ArrayList<Integer> unionOfNegatives = new ArrayList<>();
+            for (String positive : positives) { // finding the union of positives
+                if (mapOfWords.containsKey(positive)) {
+                    unionOfPositives = getUnionOverArrayLists(unionOfPositives, mapOfWords.get(positive));
+                }
+            }
+            for (String negative : negatives) { // finding the union of negatives
+                if (mapOfWords.containsKey(negative)) {
+                    unionOfNegatives = getUnionOverArrayLists(unionOfNegatives, mapOfWords.get(negative));
+                }
+            }
+            result = getSubtractionFromArrayLists(getIntersectionOverArrayLists(result, unionOfPositives), unionOfNegatives);
+        }
+        return result;
+    }
+
+    private static ArrayList<Integer> getIntersectionOverArrayLists(ArrayList<Integer> first, ArrayList<Integer> second) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int fileNumber : first) {
+            if (second.contains(fileNumber))
+                result.add(fileNumber);
+        }
+        return result;
+    }
+
+    private static ArrayList<Integer> getUnionOverArrayLists(ArrayList<Integer> first, ArrayList<Integer> second) {
+        ArrayList<Integer> result = new ArrayList<>(first);
+        for (Integer fileNumber : second)
+            if (!result.contains(fileNumber))
+                result.add(fileNumber);
+        return result;
+    }
+
+    private static ArrayList<Integer> getSubtractionFromArrayLists(ArrayList<Integer> first, ArrayList<Integer> second) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (Integer fileNumber : first) {
+            if (!second.contains(fileNumber))
+                result.add(fileNumber);
+        }
+        return result;
+    }
 
     private static void readAllFilesAndSaveWords() {
         mapOfWords = new HashMap<>();
@@ -138,7 +143,7 @@ public class Main {
             BufferedReader br = new BufferedReader(new FileReader("src/stop words/stop_words_english.txt"));
             String word;
             while ((word = br.readLine()) != null) {
-                allStopWords.add(word.toUpperCase().replaceAll("\\p{Punct}",""));
+                allStopWords.add(word.toUpperCase().replaceAll("\\p{Punct}", ""));
             }
         } catch (IOException e) {
             e.printStackTrace();
