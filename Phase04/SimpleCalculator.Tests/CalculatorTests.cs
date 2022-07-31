9f785bc70156a3fa0d0f21456fce75ccfc866df6
DnyaNvB <dnya.nvb@gmail.com>
2022-07-31 16:58:23 +0430

    phase04

---
+++ Phase04/SimpleCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,51 @@
+using SimpleCalculator.Business;
+using SimpleCalculator.Business.Enums;
+
+namespace SimpleCalculator.Tests;
+
+public class CalculatorUnitTest
+{
+    private Calculator _calculator = new Calculator();
+
+    [Theory]
+    [InlineData(0, 2, 2)]
+    [InlineData(-2, -2, -4)]
+    [InlineData(-2000, 1000, -1000)]
+    public void SumCalculation_Equality_Test(int first, int second, int answer)
+    {
+        Assert.Equal(_calculator.Calculate(first, second, OperatorEnum.sum), answer);
+    }
+
+    [Theory]
+    [InlineData(0, 2, -2)]
+    [InlineData(-2, -2, 0)]
+    [InlineData(-2000, 1000, -3000)]
+    public void SubCalculation_Equality_Test(int first, int second, int answer)
+    {
+        Assert.Equal(_calculator.Calculate(first, second, OperatorEnum.sub), answer);
+    }
+
+    [Theory]
+    [InlineData(0, 2, 0)]
+    [InlineData(-2, -2, 4)]
+    [InlineData(-2000, 1000, -2000000)]
+    public void MultiplyCalculation_Equality_Test(int first, int second, int answer)
+    {
+        Assert.Equal(_calculator.Calculate(first, second, OperatorEnum.multiply), answer);
+    }
+
+    [Theory]
+    [InlineData(-2, -2, 1)]
+    [InlineData(-2000, 1000, -2)]
+    public void DivisionCalculation_Equality_Test(int first, int second, int answer)
+    {
+        Assert.Equal(_calculator.Calculate(first, second, OperatorEnum.division), answer);
+    }
+
+    [Fact]
+    public void DivisionByZero_ExceptionHandling_Test()
+    {
+        Action act = () => _calculator.Calculate(10, 0, OperatorEnum.division), answer;
+        Assert.Throws<DivideByZeroException>(act);
+    }
+}
